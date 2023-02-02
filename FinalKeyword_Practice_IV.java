7da78db7a0afb6b99b71f2da3180c1fdae26dca1
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-02 21:52:17 +0530

    Add files via upload

---
+++ FinalKeyword_Practice_IV.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class FinalKeyword_Practice_IV {
+
+	public static void main(String[] args) 
+	{
+		// Final Keyword
+		
+		final int year = 2023;
+		//year = 2022;
+		System.out.println(year);
+		
+		
+	}
+
+}
