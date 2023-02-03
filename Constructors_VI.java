df1f50c4d65beaca744189fc27079fdb8da06ec6
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-03 15:49:57 +0530

    Add files via upload

---
+++ Constructors_VI.java
@@ -0,0 +1,49 @@
+package practice;
+
+public class Constructors_VI {
+	// String A = "This is default Constructor" ;
+//====================================================================================================================
+
+	// No Argument Constructor
+	
+	/*public Constructors_VI() {
+		String A = "This is no Argument Constructor";
+		System.out.println(A);
+	}
+*/
+	
+//===================================================================================================================
+	//Parameterized Constructor
+	/*
+	Constructors_VI(int a , int b )
+	{
+		System.out.println("this is parameterized Constructor");
+		System.out.println(a + " " + b);
+		System.out.println(a + b);
+	}
+	*/
+	
+	
+	
+	
+	
+//========================================================================================================================
+	public static void main(String[] args) {
+		// Default Constructor
+		  /*Constructors_VI Def = new Constructors_VI(); 
+		  System.out.println(Def.A);
+		 */
+
+//=========================================================================================================================		
+		//No Argument Constructor
+		//Constructors_VI Def = new Constructors_VI();
+//=================================================================================================================
+		//Parameterized Constructors
+		//Constructors_VI Def = new Constructors_VI(12, 122);
+		
+//=================================================================================================================		
+		
+		
+	}
+
+}
