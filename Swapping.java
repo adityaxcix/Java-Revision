83454e8dd6b4382f9ab6058a31ab49dd595fd61b
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-09 14:32:13 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,21 @@
+package programs;
+import java.util.*;
+public class Swapping 
+{
+
+	public static void main(String[] args)
+	{
+		  int x, y, temp;
+	        System.out.println("Enter x and y");
+	        Scanner in = new Scanner(System.in);
+	        x = in.nextInt();
+	        y = in.nextInt();
+	        System.out.println("Before Swapping " + x+ " " + y);
+	        temp = x;
+	        x = y;
+	        y = temp;
+	        System.out.println("After Swapping " + x + " " + y);
+	         
+	}
+
+}
