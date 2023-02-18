4a905be14a4026b7557d951ba787d1e8cc1161e9
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-18 16:44:34 +0530

    Add files via upload

---
+++ Threads.java
@@ -0,0 +1,18 @@
+package prog;
+
+
+
+public class Threads {
+	public static void main(String[] args) throws Exception
+	
+	{
+		
+System.out.println("Hello i am Aditya");
+
+Thread.sleep(5000); //Thread Sleeps for 5 sec
+
+System.out.println("Thread is after executing after a NAP");
+		
+	}
+
+}
