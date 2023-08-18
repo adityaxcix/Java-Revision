08e3f5e7c3b4e76fc01e9c46822837e76f6ba036
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-08-18 17:25:30 +0530

    Add files via upload

--- Products.java
+++ Products.java
@@ -11,36 +11,46 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Products {
 
-	public static void main(String[] args) throws IOException {
-		File file = new File("C:\\Excel_ApachePOI_Sheets\\TestCase_14Aug.xlsx");
-		FileInputStream fis = new FileInputStream(file);
-		XSSFWorkbook wb = new XSSFWorkbook(fis);
-		XSSFSheet sheet = wb.getSheetAt(0);
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Excel_ApachePOI_Sheets\\SaleData.xlsx");
+        FileInputStream fis = new FileInputStream(file);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        XSSFSheet sheet = wb.getSheetAt(0);
 
-		for (Row row : sheet) 
-		{
-			for (Cell cell : row)
-			{
-				switch (cell.getCellType()) 
-				{
+        for (Row row : sheet) {
+            for (Cell cell : row) {
+                String cellValue = "";
+                switch (cell.getCellType()) {
+                    case NUMERIC:
+                        cellValue = String.valueOf(cell.getNumericCellValue());
+                        break;
+                    case BOOLEAN:
+                        cellValue = String.valueOf(cell.getBooleanCellValue());
+                        break;
+                    case STRING:
+                        cellValue = cell.getStringCellValue();
+                        break;
+                    default:
+                        break;
+                }
+                // Adjust the desired column width and spacing here
+                System.out.print(padRight(cellValue, 20) + " | ");
+            }
+            System.out.println();
+        }
 
-				case NUMERIC:
-					System.out.print(cell.getNumericCellValue());
-					break;
-				case BOOLEAN:
-					System.out.print(cell.getBooleanCellValue());
-					break;
-				case STRING:
-					System.out.print(cell.getStringCellValue());
-					break;
-				default:
-					break;
-				}
-				System.out.print("  |  " );
-			}
-			System.out.println();
+        fis.close();
+        wb.close();
+    }
 
-		}
-
-	}
+    // Function to pad the string to the right with spaces to achieve alignment
+    public static String padRight(String input, int length) {
+    	
+        StringBuilder sb = new StringBuilder(input);
+        while (sb.length() < length) 
+        {
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
